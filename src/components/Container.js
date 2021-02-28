01b4f96da18a0b09749ffb7f30252ade402aea78
Rohan-3599 <rohansinha3599@gmail.com>
2021-02-28 22:23:38 +0530

    some changes

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid } from '@material-ui/core'
+import { Grid ,Typography } from '@material-ui/core'
 import Navbar from './Navbar'
 import './styles.css'
 import { makeStyles } from '@material-ui/core/styles'
@@ -15,17 +15,57 @@ const Container = () => {
   const classes = useStyles();
     return (
     <Grid container direction="column" className={classes.container}>
-      <Grid item className="grid1">
+      <Grid item className="grid1" >
         <Navbar />
-      {/* <div className="div-gradient">
-      </div> */}
+      <div className="div-gradient">
+       <Typography style={{ fontFamily: 'Libre Baskerville', fontSize: '80px' ,fontStyle: 'normal', fontWeight: 700 , lineHeight: '96px', letterSpacing: '0em', textAlign: 'left', color: '#FFFFFF',
+}}>
+  Cari Cari
+</Typography>
+      </div>
+      <div className="div-text">
+      <Typography style={{fontFamily: 'Nunito',fontSize: '24px',fontStyle: 'normal',fontWeight: 400,lineHeight: '32px',letterSpacing: '0em',textAlign: 'left', color : '#FFFFFF'
+}}>
+  Live from their sofa to yours. Get closer to your favourite artists, and never miss out.
+</Typography>
+      </div>
+      <div className="div-circle">
+        <div className="div-circle-2">
+         <div className="circle">
+           <div className="content">
+             </div>
+         </div>
+        </div>
+        <div className="div-circle-2">
+        <div className="circle">
+           Great!!!!
+         </div>
+        </div>
+        <div className="div-circle-2">
+        <div className="circle">
+           Great!!!!
+         </div>
+        </div>
+        <div className="div-circle-2">
+        <div className="circle">
+           Great!!!!
+         </div>
+        </div>
+        
+      </div>
       </Grid >
-      <Grid item container className="grid2">
+      <Grid item container className="grid2" justify="center">
+        <div className="new-div">
+        <Grid container justify="center" spacing={3} style={{background: '#FFFFFF' }}>
+         
+        </Grid>
         {/* <Grid item xs={false} sm={2} />
         <Grid item xs={12} sm={8}>
         </Grid>
         <Grid item xs={false} sm={2} /> */}
-      </Grid>
+     
+      </div>
+    </Grid>
     </Grid>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,23 @@
-import { AppBar, Toolbar } from "@material-ui/core"
+import { AppBar, Toolbar, Typography , Grid , IconButton} from "@material-ui/core"
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
+import logo from '../images/image52.png' ;
+import SearchIcon from '@material-ui/icons/Search';
+import './styles.css'
 
-
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
     appBar: {
        background: "none" ,
        boxShadow : "none" ,
+       display: "flex" ,
+    },
+    typoGraphy : {
+        display: 'inline' ,
+     marginRight : 162,
+    },
+    SearchIcon : {
+        width: '18px',
+        height : '18px',
     }
 }))
 const Navbar = () => {
@@ -14,9 +25,37 @@ const Navbar = () => {
     return (
         <div>
             <AppBar position="static" className={classes.appBar}>
-             <Toolbar></Toolbar>
+             <Toolbar>
+                 <img src={logo} className="img-header"/>
+                <Grid container direction="row" className={classes.typoGraphy}>
+                
+                 <ul id="navbar">
+                 <IconButton  size="small" >
+                 <SearchIcon style={{color : '#FFFFFF' }} className={classes.SearchIcon}/>
+                 </IconButton >
+                     <li>
+                         
+                         <Typography style={{fontSize: '16px' ,fontFamily: 'Open Sans', fontStyle: 'normal' , lineHeight: '26px'}}>
+                
+                         Search
+                           </Typography></li>
+                    <li><Typography style={{fontSize: '16px' ,fontFamily: 'Open Sans', fontStyle: 'normal' , lineHeight: '26px'}}>
+                
+                Help
+                  </Typography></li>
+                    <li><Typography style={{fontSize: '16px' ,fontFamily: 'Open Sans', fontStyle: 'normal' , lineHeight: '26px'}}>
+                
+                Account
+                  </Typography></li>
+                      <li><Typography style={{fontSize: '16px' ,fontFamily: 'Open Sans', fontStyle: 'normal' , lineHeight: '26px'}}>
+                
+                Search
+                  </Typography></li>
+                  </ul> 
+                </Grid>
+             </Toolbar>
             </AppBar>
-        </div>
+            </div>
     )
 }
 
